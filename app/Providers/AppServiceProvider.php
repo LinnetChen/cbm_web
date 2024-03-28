7243995c13d3a1bd1525c1edff8f107f117234b1
ShaneLin <shane_lin@digeam.com>
2024-03-28 18:17:46 +0800

    use_https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use Schema;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
+
     }
 }
