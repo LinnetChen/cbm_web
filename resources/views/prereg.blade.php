5eefeb399a04d97f2a6aea8a3af2a439fca5210a
Amy_Liao <digeam110@gmail.com>
2024-04-01 14:04:48 +0800

    20240401

--- resources/views/prereg.blade.php
+++ resources/views/prereg.blade.php
@@ -18,10 +18,28 @@
     <meta name="description" content="《黑色契約MOBILE》事前預約">
     <title>《黑色契約MOBILE》事前預約</title>
     <link href="{{ mix('css/event/prereg/style.css') }}" rel="stylesheet">
-
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PB8RXNM');
+    </script>
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PB8RXNM" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="app">
         <v-prereg></v-prereg>
     </div>
